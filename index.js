60ddb32f7e9f0599f16cf8e0e94c8a166b2432c9
Sergeb250 <142254236+Sergeb250@users.noreply.github.com>
2025-05-24 06:11:13 -1100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,101 @@
+const Joi=require('joi');
+const express = require('express');
+const app=express();
+
+app.use(express.json())
+
+const courses= [
+     {id:1,name:'math'},
+     {id:2,name:'cybersec'},
+     {id:3,name:'software eng'},
+]
+
+app.get('/',(req,res)=>{
+
+    res.send('hello world');
+
+
+});
+
+
+app.get('/api/courses',(req,res)=>{
+
+res.send(courses)
+
+
+});
+
+app.get('/api/courses/:id',(req,res)=>{
+ const course= courses.find(c=> c.id === parseInt(req.params.id))
+
+ if(!course)  res.status(404).send('the cource not found');
+
+    res.send(course)
+
+});
+
+app.post('/api/courses', (req, res) => {
+  
+  const schema = Joi.object({
+    name: Joi.string().min(3).required()
+  });
+
+  const course={
+    id: courses.length +1,
+    name: req.body.name
+  };
+
+  courses.push(course);
+  res.send(course);
+});
+
+
+app.put('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+
+  if (!course) {
+    res.status(404).send('The course was not found');
+    return;
+  }
+
+  const { error } = ValidateCourse(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  course.name = req.body.name;
+  res.send(course);
+});
+
+app.delete('/api/courses/:id',(req,res)=>{
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+
+  if (!course) {
+    res.status(404).send('The course was not found');
+    return;
+  }
+
+const index= courses.indexOf(course);
+courses.splice(index,1);
+res.send(course);
+
+});
+
+function ValidateCourse(course) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required()
+  });
+
+  return schema.validate(course);
+}
+
+
+
+
+
+const port=process.env.PORT || 3000
+app.listen(port,()=> console.log(`server running on ${port}`));
+
+
 
